2418aad83209331d37d95983c34ca8a28cd90fae
KennethCortez <kennethleocortez@gmail.com>
2024-10-12 17:51:12 -0600

    A program that displays the info of a video

--- foundation/Foundation1/Program.cs
+++ foundation/Foundation1/Program.cs
@@ -4,6 +4,14 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Video video1 = new Video("How to make crepes", "Alejandra Pacheco", 1200);
+        video1._comments.Add(new Comment("Kenneth", "thar recipe taste like the one my granny used to make to me"));
+        video1._comments.Add(new Comment("Jared","What happends if i use sweetemer instead of sugar?"));
+        video1._comments.Add(new Comment("Osiris","Nice tutorial"));
+        video1._comments.Add(new Comment("Yess","I think 20 minutes of tutorial is too much, watherever those cepes were soooo good"));
+        video1._comments.Add(new Comment("Machadito","Que buenas crepas las que quedan"));
+        video1._comments.Add(new Comment("JJJk","素晴らしいチュートリアル"));
+
+        video1.ShowInfoVideo();
     }
 }
---
+++ foundation/Foundation1/Video.cs
@@ -0,0 +1,32 @@
+using System.ComponentModel.DataAnnotations;
+public class Video
+{
+    public string _title;
+    public string _author;
+    public int _durationInSeconds;
+    public List<Comment> _comments = new List<Comment>();
+    public Video(string title, string author, int durationInSeconds)
+    {
+        _title = title;
+        _author = author;
+        _durationInSeconds = durationInSeconds;
+    }
+
+    public void AddComment(string name, string text)
+    {
+        Comment newComment = new Comment(name, text);
+        _comments.Add(newComment);
+    }
+    public void ShowInfoVideo()
+    {
+        Console.WriteLine($"\nTitle: {_title}");
+        Console.WriteLine($"Author: {_author}");
+        Console.WriteLine($"Length (Seconds): {_durationInSeconds}");
+        Console.WriteLine("Number of comments; " + _comments.Count);
+        Console.WriteLine("Comments");
+        foreach (Comment newComment in _comments)
+        {
+            newComment.ShowComment();
+        }
+    }
+}
